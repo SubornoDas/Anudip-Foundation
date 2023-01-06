a674196c100b0364ab89640e61635c1ed5a8773e
SUBORNO DAS <115772385+SubornoDas@users.noreply.github.com>
2023-01-06 23:45:14 +0530

    Create First.java

---
+++ Assignments/basic/First.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class First {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0;i<5;i++)
+		{
+			System.out.println("Hello");
+		}
+		for(i=5;i>0;i--)
+		{
+			System.out.println("World");
+		}
+	}
+
+}
