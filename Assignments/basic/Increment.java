b97972b479b71372f429bf173ba0f83382250844
SUBORNO DAS <115772385+SubornoDas@users.noreply.github.com>
2023-01-06 23:47:21 +0530

    Create Increment.java

---
+++ Assignments/basic/Increment.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class Increment {
+
+	public static void main(String[] args) {
+		double var1=1+5;
+		double var2=var1/4;
+		int var3=1+5;
+		int var4=var3/4;
+		System.out.println(var2+" "+var4);
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+
