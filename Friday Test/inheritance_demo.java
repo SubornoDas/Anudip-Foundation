14b4cb3576d9f52ca9ed22158f72b1742a4f9c31
SUBORNO DAS <115772385+SubornoDas@users.noreply.github.com>
2023-01-06 23:42:20 +0530

    Create  inheritance_demo.java

---
+++ Friday Test/inheritance_demo.java
@@ -0,0 +1,26 @@
+package fridaytest;
+class A{
+	int i;
+	void display() {
+		System.out.println(i);
+	}
+}
+class B extends A{
+	int j;
+	void display() {
+		System.out.println(j);
+	}
+}
+ class inheritance_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		B obj=new B();
+		obj.i=1;
+		obj.j=2;
+		obj.display();
+		
+	}
+
+}
+ 
